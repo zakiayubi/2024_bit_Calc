575cac48002b7872ab6fb91d6c92e578ed55a717
zakiayubi <132313595+zakiayubi@users.noreply.github.com>
2024-03-27 10:30:59 +1300

    Create C_01_Statment_Generator.py

---
+++ C_01_Statment_Generator.py
@@ -0,0 +1,6 @@
+# functions go here
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+# Main routine starts here...
+statement_generator(statement="Instructions", decoration="-")
